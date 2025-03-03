d59ee1b188065b70dcefd2a4dffcf9ff1275fd77
Kwart-tech <82520576+Kwart-tech@users.noreply.github.com>
2025-03-04 00:45:31 +0100

    Update sg.tf
    security group

---
+++ sg.tf
@@ -0,0 +1,63 @@
+# Security Group for EC2 Instances
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.wordpress_vpc.id
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for ALB
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.wordpress_vpc.id
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for RDS
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.wordpress_vpc.id
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
