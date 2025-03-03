26fe4e0a32b2d47faeb19175e3a57ab36d4018a6
Kwart-tech <82520576+Kwart-tech@users.noreply.github.com>
2025-03-04 00:44:15 +0100

    Update route53.tf
    route 53 dns

---
+++ route53.tf
@@ -0,0 +1,15 @@
+resource "aws_route53_zone" "wordpress_zone" {
+  name = "mywordpressdomain.com"
+}
+
+resource "aws_route53_record" "wordpress_alb_record" {
+  zone_id = aws_route53_zone.wordpress_zone.zone_id
+  name    = "www.mywordpressdomain.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.wordpress_alb.dns_name
+    zone_id                = aws_lb.wordpress_alb.zone_id
+    evaluate_target_health = true
+  }
+}
